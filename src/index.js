d58bcbdddb60ad1d6ad8bf0fed26b7f1b3da22ef
Corina Allen <StarPerfect@gmail.com>
2019-12-17 11:36:31 -0700

    Add GET and POST requests for WordWatch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.get('https://wordwatch-api.herokuapp.com/api/v1/top_word', function(data) {
+    document.querySelector('#top_word').innerHTML = JSON.stringify(data.word)
+  });
+
+
+  document.getElementById('button').addEventListener('click', function(e) {
+    e.preventDefault();
+    var something = document.getElementById("text").value;
+    var details = `{ "word": { "value": "${something}" } }`;
+    // var details = `{ "word": { "value": "${something}" } }`;
+    console.log(details)
+    $.post('https://wordwatch-api.herokuapp.com/api/v1/words', JSON.parse(details), function(data) {
+      console.log(data)
+        // document.querySelector('#top_word').innerHTML = JSON.stringify(data.word)
+    })
+  })
 })
